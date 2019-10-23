10d604e58a37f26443e5f58f4329c091087533be
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-23 12:54:43 -0700

    Why

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,5 +1,11 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class GuestBook {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
@@ -9,4 +15,12 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton();
+	JButton view = new JButton();
+	public static void main(String[] args) {
+		ArrayList<String> names = new ArrayList<String>();
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,47 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
+		ArrayList<String> cuboid = new ArrayList<String>();
 		//   Don't forget to import the ArrayList class
 		
 		//2. Add five Strings to your list
+		cuboid.add("My");
+		cuboid.add("name");
+		cuboid.add("is");
+		cuboid.add("Bingo");
+		cuboid.add("Radioisotope");
 		
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int now = 0; now < cuboid.size(); now++) {
+			System.out.println(cuboid.get(now));
+		}
+		System.out.println("");
 		//4. Print all the Strings using a for-each loop
-		
+		for (String string : cuboid) {
+			System.out.println(string);
+		}
+		System.out.println("");
 		//5. Print only the even numbered elements in the list.
-		
+		for (int later = 0; later < cuboid.size(); later++) {
+			if (later % 2 == 1) {
+				System.out.println(cuboid.get(later));
+			}
+		}
+		System.out.println("");
 		//6. Print all the Strings in reverse order.
-		
+		for (int dont = cuboid.size() - 1; dont >= 0; dont--) {
+			System.out.println(cuboid.get(dont));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("");
+		for (int wart = 0; wart < cuboid.size(); wart++) {
+			if (cuboid.get(wart).contains("e")) {
+				System.out.println(cuboid.get(wart));
+			}
+		}
 	}
 }
